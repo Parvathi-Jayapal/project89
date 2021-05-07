0f6dfce6bf48fc144df5c8fb74b1076400b20fb5
Parvathi-Jayapal <77677826+Parvathi-Jayapal@users.noreply.github.com>
2021-05-07 19:08:08 +0530

    Add files via upload

---
+++ game_login.js
@@ -0,0 +1,10 @@
+
+function addUser(){
+  player1_name=document.getElementById("player_1_nameinput").value;
+  player2_name=document.getElementById("player_2_nameinput").value;
+
+  localStorage.setItem("player1_name");
+  localStorage.setItem("player2_name");
+
+  window.location="game_page.html";
+}
